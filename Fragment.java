08058b744078919e5b031f85a63296892a3853f4
DiasNeto27 <DiasNeto27@Gmail.com>
2016-01-10 23:21:22 -0200

    Changing Fragment class

--- Fragment.java
+++ Fragment.java
@@ -1,11 +1,16 @@
 public class Fragment {
 	private String anyProperty;
+	private String anotherProperty;
 
-	public Fragment(String value){
+	public void setAnyProperty(String value){
 		this.anyProperty = value;
 	}
 
 	public String getAnyProperty(){
 		return anyProperty;
+	}
+
+	public String getAnotherProperty(){
+		return anotherProperty;
 	}
 }
