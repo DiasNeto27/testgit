29f6fd8a977c19c48974c1d77ba3caa92e3a4725
DiasNeto27 <DiasNeto27@Gmail.com>
2016-01-10 23:17:11 -0200

    Added Fragment class

---
+++ Fragment.java
@@ -0,0 +1,11 @@
+public class Fragment {
+	private String anyProperty;
+
+	public Fragment(String value){
+		this.anyProperty = value;
+	}
+
+	public String getAnyProperty(){
+		return anyProperty;
+	}
+}
